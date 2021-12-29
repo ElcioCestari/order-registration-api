beecbd20ec1d88af40e5cf58ed4da5320dabc60b
ElcioCestari <elciotaira@gmail.com>
2021-12-29 11:05:37 -0400

    init project

---
+++ src/main/java/com/brotherselectronics/orderregistration/OrderRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.brotherselectronics.orderregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brotherselectronics/orderregistration/OrderRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotherselectronics.orderregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
