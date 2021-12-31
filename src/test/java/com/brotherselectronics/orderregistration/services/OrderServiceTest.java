a0f6d0bebbddab04c217f043a9cee70863e7be2a
ElcioCestari <elciotaira@gmail.com>
2021-12-31 15:54:38 -0400

    add tests in OrderService findById

--- src/test/java/com/brotherselectronics/orderregistration/services/OrderServiceTest.java
+++ src/test/java/com/brotherselectronics/orderregistration/services/OrderServiceTest.java
@@ -3,6 +3,7 @@ package com.brotherselectronics.orderregistration.services;
 import com.brotherselectronics.orderregistration.domains.dtos.OrderResponseDTO;
 import com.brotherselectronics.orderregistration.domains.entities.Order;
 import com.brotherselectronics.orderregistration.domains.mappers.OrderMapper;
+import com.brotherselectronics.orderregistration.exceptions.NotFoundException;
 import com.brotherselectronics.orderregistration.repositories.OrderRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,44 +15,72 @@ import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class OrderServiceTest {
+    private static final String FAKE_ID = "asdfadsfadfdasfadfafa";
 
     @InjectMocks private OrderService orderService;
     @Mock private OrderRepository orderRepository;
     @Mock private OrderMapper mapper;
     private List<Order> orderListNotEmpty;
     private List<OrderResponseDTO> dtoListNotEmpty;
+    private OrderResponseDTO orderResponseDTO;
+    private Optional<Order> orderOptional;
+    private Order order;
 
     @BeforeEach
     public void setUp() {
-        orderListNotEmpty = List.of(new Order());
+        order = new Order();
+        order.setId(FAKE_ID);
+        orderListNotEmpty = List.of(order);
         dtoListNotEmpty = List.of(new OrderResponseDTO());
+        orderResponseDTO = new OrderResponseDTO();
+        orderOptional = Optional.ofNullable(order);
     }
 
     @Test
     public void findAll_whenHasItensToReturnThenAssertThatListItsNotEmpty() {
-        Mockito.when(orderRepository.findAll()).thenReturn(orderListNotEmpty);
-        Mockito.when(mapper.toDtoResponseList(Mockito.anyList())).thenReturn(dtoListNotEmpty);
+        when(orderRepository.findAll()).thenReturn(orderListNotEmpty);
+        when(mapper.toDtoResponseList(Mockito.anyList())).thenReturn(dtoListNotEmpty);
         List<OrderResponseDTO> all = orderService.findAll();
         assertThat(all).isNotEmpty();
     }
 
     @Test
     public void findAll_whenHasNotItensToReturnThenAssertThatListItsEmpty() {
-        Mockito.when(orderRepository.findAll()).thenReturn(emptyList());
-        Mockito.when(mapper.toDtoResponseList(Mockito.anyList())).thenReturn(emptyList());
+        when(orderRepository.findAll()).thenReturn(emptyList());
+        when(mapper.toDtoResponseList(Mockito.anyList())).thenReturn(emptyList());
         List<OrderResponseDTO> all = orderService.findAll();
         assertThat(all).isEmpty();
     }
 
     @Test
-    public void findById() {
-        Assertions.assertTrue(true);
+    public void findById_whenHasOrderThenAssertThatItsNotNull() {
+        when(orderRepository.findById(anyString())).thenReturn(orderOptional);
+        when(mapper.toDtoResponse(any(Order.class))).thenReturn(orderResponseDTO);
+        OrderResponseDTO responseActual = orderService.findById(FAKE_ID);
+        assertNotNull(responseActual);
+    }
+
+    @Test
+    public void findById_whenHasNotOrderThenAssertThatItsThrowAnException() {
+        when(orderRepository.findById(anyString())).thenReturn(Optional.ofNullable(null));
+        when(mapper.toDtoResponse(any(Order.class))).thenReturn(orderResponseDTO);
+        try {
+            OrderResponseDTO responseActual = orderService.findById(FAKE_ID);
+            fail("Must to be throw an exception and don't was threw.");
+        } catch (NotFoundException e) {
+            assertThat(e.getMessage()).isEqualTo("Order not found with id: " + FAKE_ID);
+        }
     }
 
     @Test
