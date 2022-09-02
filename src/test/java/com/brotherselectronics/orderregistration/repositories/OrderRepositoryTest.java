d8e098d0f490c7dda073f47a5ee8a9a2709945e8
ElcioCestari <elciotaira@gmail.com>
2022-09-01 22:36:04 -0300

    test: OrderRepositoryTest.java

--- src/test/java/com/brotherselectronics/orderregistration/repositories/OrderRepositoryTest.java
+++ src/test/java/com/brotherselectronics/orderregistration/repositories/OrderRepositoryTest.java
@@ -1,27 +1,34 @@
 package com.brotherselectronics.orderregistration.repositories;
 
-import com.brotherselectronics.orderregistration.domains.entities.Order;
-import org.assertj.core.api.Assertions;
+import com.brotherselectronics.fakers.OrderFaker;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
 class OrderRepositoryTest {
 
     @Autowired
     private OrderRepository orderRepository;
+    private String userId;
 
     @BeforeEach
     void setUp() {
+        var order = orderRepository.save(new OrderFaker().getEntity());
+        this.userId = order.getUserId();
     }
 
     @Test
     void findByUserId() {
-        List<Order> orders = orderRepository.findByUserId("61d4afeb2c9551533eb2e74a");
-        Assertions.assertThat(orders).isNotEmpty();
+        assertThat(orderRepository.findByUserId(userId)).isNotEmpty();
+    }
+
+    @AfterEach
+    void tearDown() {
+        orderRepository.deleteAll();
     }
 }
