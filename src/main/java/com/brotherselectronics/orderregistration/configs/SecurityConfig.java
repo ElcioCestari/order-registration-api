41c207a88d96f5e1313ba758c744eea997e92875
ElcioCestari <elciotaira@gmail.com>
2022-09-01 21:00:58 -0300

    refactor: security - extract roles to config

--- src/main/java/com/brotherselectronics/orderregistration/configs/SecurityConfig.java
+++ src/main/java/com/brotherselectronics/orderregistration/configs/SecurityConfig.java
@@ -3,9 +3,6 @@ package com.brotherselectronics.orderregistration.configs;
 import com.brotherselectronics.orderregistration.services.MyUserDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.http.HttpMethod.*;
 
@@ -28,38 +24,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .httpBasic()
-//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .csrf().disable()
-//                .and()
                 .authorizeRequests()
-//                .antMatchers("/home", "/")
-//                .permitAll()
-//                .antMatchers(GET, "/**").hasAnyRole("USER","ADMIN")
-//                .antMatchers(POST, "/**").hasAnyRole("ADMIN")
-//                .antMatchers(PUT, "/**").hasAnyRole("ADMIN")
-//                .antMatchers(DELETE, "/**").hasAnyRole("ADMIN")
+                .antMatchers("/home", "/")
+                .permitAll()
+                .antMatchers(GET, "/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(POST, "/**").hasAnyRole("ADMIN")
+                .antMatchers(PUT, "/**").hasAnyRole("ADMIN")
+                .antMatchers(DELETE, "/**").hasAnyRole("ADMIN")
                 .anyRequest()
                 .authenticated()
-//                .denyAll()
-//                .permitAll()
                 .and().httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//        auth.inMemoryAuthentication()
-//                .withUser("elcio")
-//                .password(encoder.encode("elcio"))
-//                .roles("ADMIN")
-//                .and()
-//                .withUser("maria")
-//                .password(encoder.encode("maria"))
-//                .roles("USER");
-
-        log.info(encoder.encode("elcio"));
-
         auth.userDetailsService(userDetailService).passwordEncoder(encoder);
     }
 }
--- src/main/java/com/brotherselectronics/orderregistration/controllers/OrderController.java
+++ src/main/java/com/brotherselectronics/orderregistration/controllers/OrderController.java
@@ -2,52 +2,49 @@ package com.brotherselectronics.orderregistration.controllers;
 
 
 import com.brotherselectronics.orderregistration.domains.dtos.OrderRequestDTO;
+import com.brotherselectronics.orderregistration.domains.dtos.OrderResponseDTO;
 import com.brotherselectronics.orderregistration.services.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
-    @Autowired
-    private OrderService service;
+    private final OrderService service;
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN')")
-    public ResponseEntity<?> findById(@PathVariable("id") String id){
-        return ResponseEntity.ok(service.findById(id));
+    public ResponseEntity<OrderResponseDTO> findById(@PathVariable("id") String id) {
+        return ok(service.findById(id));
     }
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('ADMIN')")
-    public ResponseEntity<?> findAll(@AuthenticationPrincipal UserDetails userDetails){
-        System.out.println("userDetails = " + userDetails);
-        return ResponseEntity.ok(service.findAll());
+    public ResponseEntity<List<OrderResponseDTO>> findAll() {
+        return ok(service.findAll());
     }
 
     @PostMapping
-    @PreAuthorize("hasAnyRole('ADMIN')")
-    public ResponseEntity<?> save(@Valid @RequestBody OrderRequestDTO dto) {
-        return ResponseEntity.ok(service.save(dto));
+    public ResponseEntity<OrderResponseDTO> save(@Valid @RequestBody OrderRequestDTO dto) {
+        return ok(service.save(dto));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@Valid @RequestBody OrderRequestDTO dto, @PathVariable String id) {
-        return ResponseEntity.ok(service.update(dto,id));
+    public ResponseEntity<OrderResponseDTO> update(@Valid @RequestBody OrderRequestDTO dto, @PathVariable String id) {
+        return ok(service.update(dto, id));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable String id) {
         service.delete(id);
-        return ResponseEntity.ok(HttpStatus.ACCEPTED);
+        return ok(OK);
     }
 
 }
