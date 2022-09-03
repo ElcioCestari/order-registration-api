c361e9e650a499b2e0e16541b90defb73a01bf05
ElcioCestari <elciotaira@gmail.com>
2022-09-03 13:39:19 -0300

    test: used @SpringBootTest and mock mockMvc

--- src/test/java/com/brotherselectronics/configs/TestConfig.java
+++ src/test/java/com/brotherselectronics/configs/TestConfig.java
@@ -6,6 +6,12 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * This class it's a configuration to MyUserDetailService
+ * it was deprecated because it's not more necessarily,
+ * was not removed yet just to serve like a reference if necessary to do another configuration
+ */
+@Deprecated(since = "2022-09-03")
 @TestConfiguration
 public class TestConfig {
     @Mock
--- src/test/java/com/brotherselectronics/orderregistration/controllers/OrderControllerTest.java
+++ src/test/java/com/brotherselectronics/orderregistration/controllers/OrderControllerTest.java
@@ -1,8 +1,6 @@
 package com.brotherselectronics.orderregistration.controllers;
 
-import com.brotherselectronics.configs.TestConfig;
 import com.brotherselectronics.fakers.OrderFaker;
-import com.brotherselectronics.orderregistration.configs.SecurityConfig;
 import com.brotherselectronics.orderregistration.domains.constraints.ExistsValidator;
 import com.brotherselectronics.orderregistration.domains.dtos.OrderRequestDTO;
 import com.brotherselectronics.orderregistration.domains.dtos.OrderResponseDTO;
@@ -14,15 +12,13 @@ import com.brotherselectronics.orderregistration.utils.InstancesUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
@@ -34,107 +30,120 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = OrderController.class)
-//@SpringBootTest
-@AutoConfigureWebTestClient
-@Import(TestConfig.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderControllerTest {
     private static final String END_POINT = "/orders";
 
-    @Autowired private MockMvc mockMvc;
-    @MockBean private OrderService orderService;
-//    @MockBean private ExistsValidator existsValidator;
-//    @MockBean private InstancesUtils instancesUtils;
-//    @MockBean private BaseRepository orderRepository;
-//
-//    private List<OrderResponseDTO> orderResponseDTOListNotEmpty;
-//    private OrderResponseDTO responseDTOStub;
-//    private OrderRequestDTO requestDTOStub;
-//    private Order orderStub;
-//
-//    private String jsonList;
-//    private String jsonRequestDTOStub;
-//    private String jsonResponseDTOStub;
-//    private OrderFaker orderFaker;
-//
-//    @BeforeEach
-//    public void setup() {
-//        orderFaker = new OrderFaker();
-//        responseDTOStub = orderFaker.getResponseDTO();
-//        orderResponseDTOListNotEmpty = (List<OrderResponseDTO>) orderFaker.getResponseDTOCollection();
-//        jsonList = convertObjectToString(orderResponseDTOListNotEmpty);
-//        jsonResponseDTOStub = convertObjectToString(responseDTOStub);
-//        requestDTOStub = orderFaker.getRequestDTO();
-//        jsonRequestDTOStub = convertObjectToString(requestDTOStub);
-//        orderStub = orderFaker.getEntity();
-//    }
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+    @MockBean
+    private OrderService orderService;
+    @MockBean
+    private ExistsValidator existsValidator;
+    @MockBean
+    private InstancesUtils instancesUtils;
+    @MockBean
+    private BaseRepository orderRepository;
+
+    private List<OrderResponseDTO> orderResponseDTOListNotEmpty;
+    private OrderResponseDTO responseDTOStub;
+    private OrderRequestDTO requestDTOStub;
+    private Order orderStub;
+
+    private String jsonList;
+    private String jsonRequestDTOStub;
+    private String jsonResponseDTOStub;
+    private OrderFaker orderFaker;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+        orderFaker = new OrderFaker();
+        responseDTOStub = orderFaker.getResponseDTO();
+        orderResponseDTOListNotEmpty = (List<OrderResponseDTO>) orderFaker.getResponseDTOCollection();
+        jsonList = convertObjectToString(orderResponseDTOListNotEmpty);
+        jsonResponseDTOStub = convertObjectToString(responseDTOStub);
+        requestDTOStub = orderFaker.getRequestDTO();
+        jsonRequestDTOStub = convertObjectToString(requestDTOStub);
+        orderStub = orderFaker.getEntity();
+    }
 
     @Test
     public void findById() {
         assertThat(true);
     }
-//
-//    @Test
-//    public void findAll_whenHasItensThenReturnAnJsonList() throws Exception {
-//        when(orderService.findAll()).thenReturn(orderResponseDTOListNotEmpty);
-//        mockMvc.perform(MockMvcRequestBuilders.get(END_POINT))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void findAll_whenHasNoItensThenReturnAnEmptyJson() throws Exception {
-//        when(orderService.findAll()).thenReturn(emptyList());
-//        mockMvc.perform(MockMvcRequestBuilders.get(END_POINT))
-//                .andExpect(status().isOk())
-//                .andExpect(content().json("[]"));
-//    }
-//
-//    @Test
-//    public void save_whenReceiveAValidRequestDTOThenReturnAResponseDTO() throws Exception {
-//        when(orderService.save(any(OrderRequestDTO.class))).thenReturn(responseDTOStub);
-//        when(instancesUtils.getRepository(any())).thenReturn(this.orderRepository);
-//        when(orderRepository.findById(anyString())).thenReturn(Optional.ofNullable(orderFaker));
-//        mockMvc.perform(post(END_POINT)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(jsonRequestDTOStub))
-//                .andExpect(status().isOk());
-//
-//    }
-//
-//    @Test
-//    public void update_whenReceiveAValidRequestDTOThenReturnAResponseDTO() throws Exception {
-//        String fakeId = "any id";
-//        when(orderService.update(any(OrderRequestDTO.class), anyString())).thenReturn(responseDTOStub);
-//        when(instancesUtils.getRepository(any())).thenReturn(this.orderRepository);
-//        when(orderRepository.findById(anyString())).thenReturn(Optional.ofNullable(orderFaker));
-//
-//        mockMvc.perform(put(END_POINT+"/"+fakeId)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(jsonRequestDTOStub))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void update_whenReceiveAnInvalidRequestThenReturnANotFound() throws Exception {
-//        String fakeId = "any id";
-//        String msg = "Resource not found with id: " + fakeId;
-//        when(orderService.update(any(OrderRequestDTO.class), anyString())).thenThrow(new NotFoundException((msg)));
-//        when(instancesUtils.getRepository(any())).thenReturn(this.orderRepository);
-//        when(orderRepository.findById(anyString())).thenReturn(Optional.ofNullable(orderFaker));
-//
-//        mockMvc.perform(put(END_POINT+"/"+fakeId)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(jsonRequestDTOStub))
-//                .andExpect(status().isNotFound());
-//    }
-//
-//    @Test
-//    public void delete(){
-//        assertThat(true);
-//    }
+
+    @Test
+    @WithMockUser("spring")
+    public void findAll_whenHasItemsThenReturnAnJsonList() throws Exception {
+        when(orderService.findAll()).thenReturn(orderResponseDTOListNotEmpty);
+        mockMvc.perform(MockMvcRequestBuilders.get(END_POINT))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser("spring")
+    public void findAll_whenHasNoItemsThenReturnAnEmptyJson() throws Exception {
+        when(orderService.findAll()).thenReturn(emptyList());
+        mockMvc.perform(MockMvcRequestBuilders.get(END_POINT))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @WithMockUser("spring")
+    public void save_whenReceiveAValidRequestDTOThenReturnAResponseDTO() throws Exception {
+        when(orderService.save(any(OrderRequestDTO.class))).thenReturn(responseDTOStub);
+        when(instancesUtils.getRepository(any())).thenReturn(this.orderRepository);
+        when(orderRepository.findById(anyString())).thenReturn(Optional.ofNullable(orderFaker));
+        mockMvc.perform(post(END_POINT)
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonRequestDTOStub))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    @WithMockUser("spring")
+    public void update_whenReceiveAValidRequestDTOThenReturnAResponseDTO() throws Exception {
+        String fakeId = "any id";
+        when(orderService.update(any(OrderRequestDTO.class), anyString())).thenReturn(responseDTOStub);
+        when(instancesUtils.getRepository(any())).thenReturn(this.orderRepository);
+        when(orderRepository.findById(anyString())).thenReturn(Optional.ofNullable(orderFaker));
+
+        mockMvc.perform(put(END_POINT + "/" + fakeId)
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonRequestDTOStub))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser("spring")
+    public void update_whenReceiveAnInvalidRequestThenReturnANotFound() throws Exception {
+        String fakeId = "any id";
+        String msg = "Resource not found with id: " + fakeId;
+        when(orderService.update(any(OrderRequestDTO.class), anyString())).thenThrow(new NotFoundException((msg)));
+        when(instancesUtils.getRepository(any())).thenReturn(this.orderRepository);
+        when(orderRepository.findById(anyString())).thenReturn(Optional.ofNullable(orderFaker));
+
+        mockMvc.perform(put(END_POINT + "/" + fakeId)
+                        .contentType(APPLICATION_JSON)
+                        .content(jsonRequestDTOStub))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser("spring")
+    public void delete() {
+        assertThat(true);
+    }
 }
