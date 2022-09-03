d6d37b860258a5723b7252fdbc9178f16c9390b1
ElcioCestari <elciotaira@gmail.com>
2022-09-03 16:00:36 -0300

    test: ad @WithMockUser in ProductControllerTest.java

--- src/test/java/com/brotherselectronics/orderregistration/controllers/ProductControllerTest.java
+++ src/test/java/com/brotherselectronics/orderregistration/controllers/ProductControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -11,6 +12,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class ProductControllerTest {
@@ -29,24 +32,31 @@ class ProductControllerTest {
     }
 
     @Test
+    @WithMockUser
     void findById() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(END_POINT))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(get(END_POINT))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void findAll() {
+    @WithMockUser
+    void findAll() throws Exception {
+        mockMvc.perform(get(END_POINT))
+                .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     void save() {
     }
 
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     void update() {
     }
 
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     void delete() {
     }
 }
