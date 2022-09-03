9c246d35ff2451a1b3479dfa4f524849975f3425
ElcioCestari <elciotaira@gmail.com>
2022-09-03 15:57:44 -0300

    test: fix to load context and mock spring security

--- src/test/java/com/brotherselectronics/orderregistration/controllers/ProductControllerTest.java
+++ src/test/java/com/brotherselectronics/orderregistration/controllers/ProductControllerTest.java
@@ -1,45 +1,52 @@
 package com.brotherselectronics.orderregistration.controllers;
 
-import com.brotherselectronics.orderregistration.services.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@WebMvcTest(controllers = ProductController.class)
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@SpringBootTest
 class ProductControllerTest {
     private static final String END_POINT = "/products";
 
-    @Autowired private MockMvc mockMvc;
-    @MockBean private ProductService service;
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
-    public void findById() throws Exception {
+    void findById() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(END_POINT))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    public void findAll() {
+    void findAll() {
     }
 
     @Test
-    public void save() {
+    void save() {
     }
 
     @Test
-    public void update() {
+    void update() {
     }
 
     @Test
-    public void delete() {
+    void delete() {
     }
 }
